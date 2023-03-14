047ca8afb20df66136f3b5167c1d75fa7ad503c4
Martin PCescritorio <bergesio90@gmail.com>
2023-03-14 21:41:31 +0100

    39. Switch - 40. While y Do While

---
+++ 01-fundamentos/assets/js/ciclos/while-do-while.js
@@ -0,0 +1,20 @@
+const carros = ["Ford", "Mazda", "Honda", "Toyota"];
+let i = 0;
+console.warn("While");
+while (i < carros.length) {
+  console.log(carros[i]);
+  i++;
+}
+
+while (carros[i]) {
+  console.log(carros[i]);
+  i++;
+}
+
+console.warn("Do while");
+
+let j = 0;
+do {
+  console.log(carros[j]);
+  j++;
+} while (carros[j]);
