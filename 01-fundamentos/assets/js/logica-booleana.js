c5cc77f43adfc5524892547d68399f7fc5dfccd6
Martin Bergesio Trabajo <bergesio90@gmail.com>
2023-03-14 11:59:06 +0100

    36. Pro tip Asignaciones con operadores

--- 01-fundamentos/assets/js/logica-booleana.js
+++ 01-fundamentos/assets/js/logica-booleana.js
@@ -33,3 +33,15 @@ console.log(regresaTrue() || regresaFalse());
 console.log(regresaFalse() || regresaTrue());
 
 console.warn("Asignaciones");
+
+const soyUndefined = undefined;
+const soyNull = null;
+const soyFalse = false;
+
+const a1 = false && "Hola mundo" && 150; // Evalua si todos los valore son true y asigna el último a la variable, si un valor es false le asigna false a la variable
+const a2 = "Hola" && "Mundo" && soyFalse && true; // Evalua si todos los valore son true y asigna el último a la variable, si un valor es false le asigna false a la variable
+const a3 = soyFalse || "Ya no soy falso"; // Le asigna el primer valor que sea true
+const a4 = soyFalse || soyUndefined || soyNull || "Ya no soy falso de nuevo" || true; // Le asigna el primer valor que sea true
+const a5 = soyFalse || soyUndefined || regresaTrue() || "Ya no soy falso de nuevo" || true; // Le asigna el primer valor que sea true
+
+console.log({ a1, a2, a3, a4, a5 });
