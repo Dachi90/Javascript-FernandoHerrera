3ce407f9a06199099d83ce541a377aaa635681f3
Martin PCescritorio <bergesio90@gmail.com>
2023-03-19 20:51:36 +0100

    68. Code Minify

--- 02-blackjack/assets/js/juego.js
+++ 02-blackjack/assets/js/juego.js
@@ -1,4 +1,4 @@
-(() => {
+const miModulo = (() => {
   'use strict';
 
   let deck = [];
@@ -17,9 +17,17 @@
   // Esta función inicializa el juego
   const inicializarJuego = (numJugadores = 2) => {
     deck = crearDeck();
+    puntosJugadores = [];
     for (let i = 0; i < numJugadores; i++) {
       puntosJugadores.push(0);
     }
+
+    puntosHTML.forEach((elem) => (elem.innerText = 0));
+
+    divCartasJugadores.forEach((elem) => (elem.innerHTML = ''));
+
+    btnPedir.disabled = false;
+    btnDetener.disabled = false;
   };
 
   // Esta función crea un nuevo deck
@@ -67,17 +75,8 @@
     divCartasJugadores[turno].append(imgCarta);
   };
 
-  const turnoComputadora = (puntosMinimos) => {
-    let puntosComputadora = 0;
-    do {
-      const carta = pedirCarta();
-      puntosComputadora = acumularPuntos(carta, puntosJugadores.length - 1);
-      crearCarta(carta, puntosJugadores.length - 1);
-
-      if (puntosMinimos > 21) {
-        break;
-      }
-    } while (puntosComputadora < puntosMinimos && puntosMinimos <= 21);
+  const determinarGanador = () => {
+    const [puntosMinimos, puntosComputadora] = puntosJugadores;
 
     setTimeout(() => {
       if (puntosComputadora === puntosMinimos) {
@@ -90,6 +89,16 @@
         alert('Computadora Gana');
       }
     }, 100);
+  };
+
+  const turnoComputadora = (puntosMinimos) => {
+    let puntosComputadora = 0;
+    do {
+      const carta = pedirCarta();
+      puntosComputadora = acumularPuntos(carta, puntosJugadores.length - 1);
+      crearCarta(carta, puntosJugadores.length - 1);
+    } while (puntosComputadora < puntosMinimos && puntosMinimos <= 21);
+    determinarGanador();
   };
 
   // Eventos
@@ -116,25 +125,14 @@
     btnPedir.disabled = true;
     btnDetener.disabled = true;
 
-    turnoComputadora(puntosJugador);
+    turnoComputadora(puntosJugadores[0]);
   });
 
-  btnNuevo.addEventListener('click', () => {
-    console.clear();
+  /*   btnNuevo.addEventListener('click', () => {
     inicializarJuego();
-    /* deck = [];
-    deck = crearDeck(); */
+  }); */
 
-    /*   puntosJugador = 0;
-    puntosComputadora = 0; */
-
-    /* puntosHTML[0].innerText = 0;
-    puntosHTML[1].innerText = 0;
-
-    divCartasComputadora.innerHTML = '';
-    divCartasJugador.innerHTML = '';
-
-    btnPedir.disabled = false;
-    btnDetener.disabled = false; */
-  });
+  return {
+    nuevoJuego: inicializarJuego,
+  };
 })();
