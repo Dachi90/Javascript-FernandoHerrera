d88b98081f7d8f536ff4990609f28e3b09651cd1
Martin Bergesio Trabajo <bergesio90@gmail.com>
2023-03-24 14:14:44 +0100

    91. Mejorar la documentación JSDoc comments

--- 04-blackjack-vite/src/blackjack/usecases/crear-deck.js
+++ 04-blackjack-vite/src/blackjack/usecases/crear-deck.js
@@ -1,8 +1,17 @@
 import _ from 'underscore';
 
-export const miNombre = 'Fernando';
+//export const miNombre = 'Fernando';
 
+/**
+ *Eta función crea un nuevo deck
+ * @param {Array<String>} tiposDeCarta Ejemplo : ['C', 'D', 'H', 'S']
+ * @param {Array<String>} tipoEspeciales Ejemplo: ['A', 'J', 'Q', 'K']
+ * @returns {Array<String>} retorna un nuevo deck de cartas
+ */
 export const crearDeck = (tiposDeCarta, tipoEspeciales) => {
+  if (!tiposDeCarta || tiposDeCarta.length === 0) throw new Error('tiposDeCarta es obligatorio, como un arreglo de string');
+  if (!tipoEspeciales || tipoEspeciales.length === 0) throw new Error('tipoEspeciales es obligatorio, como un arreglo de string');
+
   let deck = [];
   for (let i = 2; i <= 10; i++) {
     for (let tipo of tiposDeCarta) {
