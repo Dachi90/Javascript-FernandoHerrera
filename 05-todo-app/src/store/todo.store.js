2db68b3b3cd743e5241a65775953e3ac50a232c7
Martin PCescritorio <bergesio90@gmail.com>
2023-03-30 17:45:44 +0200

    111. Métodos que ocuparemos del Store

--- 05-todo-app/src/store/todo.store.js
+++ 05-todo-app/src/store/todo.store.js
@@ -16,6 +16,48 @@ const initStore = () => {
   console.log(state);
 };
 
+const loadStore = () => {
+  throw new Error('Not implemented');
+};
+
+/**
+ *
+ * @param {String} description
+ */
+const addTodo = (description) => {
+  throw new Error('Not implemented');
+};
+
+/**
+ *
+ * @param {String} todoId
+ */
+const toggleTodo = (todoId) => {
+  throw new Error('Not implemented');
+};
+
+const deleteTodo = (todoId) => {
+  throw new Error('Not implemented');
+};
+
+const deleteCompleted = () => {
+  throw new Error('Not implemented');
+};
+
+const setFilter = (newFilter = Filters.All) => {
+  throw new Error('Not implemented');
+};
+
+const getCurrentFilter = () => {
+  throw new Error('Not implemented');
+};
+
 export default {
   initStore,
+  loadStore,
+  toggleTodo,
+  deleteTodo,
+  deleteCompleted,
+  setFilter,
+  getCurrentFilter,
 };
