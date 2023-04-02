6548cd7d9ee0c9743d1a3c893edf59c56cee0d50
Martin PCescritorio <bergesio90@gmail.com>
2023-04-02 22:31:53 +0200

    114. Renderizar listado de Todos

--- 05-todo-app/src/todos/app.js
+++ 05-todo-app/src/todos/app.js
@@ -1,14 +1,26 @@
+import todoStore from '../store/todo.store';
 import html from './app.html?raw';
+import { renderTodos } from './use-cases';
+
+const ElementIDs = {
+	TodoList: '.todo-list',
+};
 
 /**
  *
  * @param {String} elementId Elemento donde vamos a renderizar la aplicacion
  */
 export const App = (elementId) => {
-	// Cuando la funciñon App() se llama
+	const displayTodos = () => {
+		const todos = todoStore.getTodos(todoStore.getCurrentFilter());
+		renderTodos(ElementIDs.TodoList, todos);
+	};
+
+	// Cuando la función App() se llama
 	(() => {
 		const app = document.createElement('div');
 		app.innerHTML = html;
 		document.querySelector(elementId).append(app);
+		displayTodos();
 	})();
 };
