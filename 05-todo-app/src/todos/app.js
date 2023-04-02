58abfc4452428f4a4141c62849a0a016adb72e26
Martin PCescritorio <bergesio90@gmail.com>
2023-04-02 19:17:05 +0200

    Eslint + Prettier config

--- 05-todo-app/src/todos/app.js
+++ 05-todo-app/src/todos/app.js
@@ -5,10 +5,10 @@ import html from './app.html?raw';
  * @param {String} elementId Elemento donde vamos a renderizar la aplicacion
  */
 export const App = (elementId) => {
-  // Cuando la funciñon App() se llama
-  (() => {
-    const app = document.createElement('div');
-    app.innerHTML = html;
-    document.querySelector(elementId).append(app);
-  })();
+	// Cuando la funciñon App() se llama
+	(() => {
+		const app = document.createElement('div');
+		app.innerHTML = html;
+		document.querySelector(elementId).append(app);
+	})();
 };
