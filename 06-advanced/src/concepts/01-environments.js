38f5e6f1b653d86fac3e5aaaa9d88dc535d89367
Martin Bergesio Trabajo <bergesio90@gmail.com>
2023-04-06 11:11:54 +0200

    128. Variables de entorno

---
+++ 06-advanced/src/concepts/01-environments.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * @param {HTMLDivElement} element
+ */
+export const environmentsComponent = (element) => {
+	console.log(import.meta.env);
+
+	const html = `
+    Dev: ${import.meta.env.DEV} </br>
+    Prod: ${import.meta.env.PROD} </br>
+    Api Key: ${import.meta.env.VITE_API_KEY} </br>
+    Base URL: ${import.meta.env.VITE_BASE_URL} </br>
+  `;
+
+	element.innerHTML = html;
+};
