4d2dc1d830115d5546e82ca0a83a03fc55c9cc05
Martin PCescritorio <bergesio90@gmail.com>
2023-04-09 12:58:05 +0200

    134. Utilizar una promesa

--- 06-advanced/src/concepts/03-promises.js
+++ 06-advanced/src/concepts/03-promises.js
@@ -4,7 +4,25 @@ import { heroes } from '../data/heroes';
  *
  * @param {HTMLDivElement} element
  */
-export const promiseComponent = (element) => {};
+export const promiseComponent = (element) => {
+	const renderHero = (hero) => {
+		element.innerHTML = hero.name;
+	};
+
+	const renderError = (error) => {
+		element.innerHTML = ` <h3>${error}</h3>`;
+	};
+
+	const id1 = '5d86371f25a058e5b1c8a65ea';
+	// findHero(id1)
+	// 	.then((superHero) => {
+	// 		renderHero(superHero);
+	// 	})
+	// 	.catch((error) => renderError(error));
+
+	// Si en el then llamamos a otra función a la que le vamos a pasar exactamente los mismos parámetros podemos abreviarlo de esta manera.
+	findHero(id1).then(renderHero).catch(renderError);
+};
 
 /**
  *
