9faad883c07d7f970b07ff1f576d6c88dcb5fe3c
Martin PCescritorio <bergesio90@gmail.com>
2023-04-10 18:30:51 +0200

    138. Async

--- 06-advanced/main.js
+++ 06-advanced/main.js
@@ -4,6 +4,7 @@ import { environmentsComponent } from './src/concepts/01-environments.js';
 import { callbackComponents } from './src/concepts/02-callbacks';
 import { promiseComponent } from './src/concepts/03-promises';
 import { promiseRaceComponent } from './src/concepts/04-promise.race';
+import { asycnComponent } from './src/concepts/05-async';
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -27,4 +28,5 @@ const element = document.querySelector('.card');
 //environmentsComponent(element);
 //callbackComponents(element);
 //promiseComponent(element);
-promiseRaceComponent(element);
+//promiseRaceComponent(element);
+asycnComponent(element);
