d3d16616420b6d2eafe98e79f4200d3dbc3747fd
Martin PCescritorio <bergesio90@gmail.com>
2023-04-14 17:10:11 +0200

    160. Store centrarl de nuestra informacion

---
+++ 07-http-app/src/users/store/users.store.js
@@ -0,0 +1,30 @@
+const state = {
+	currantPage: 0,
+	users: [],
+};
+
+const loadNextPage = async () => {
+	throw new Error('No implementado');
+};
+
+const loadPreviousPage = async () => {
+	throw new Error('No implementado');
+};
+
+const onUserChange = () => {
+	throw new Error('No implementado');
+};
+
+const reloadPage = async () => {
+	throw new Error('No implementado');
+};
+
+export default {
+	loadNextPage,
+	loadPreviousPage,
+	onUserChange,
+	reloadPage,
+
+	getUser: () => [...state.users],
+	getCurrentPage: () => state.currantPage,
+};
