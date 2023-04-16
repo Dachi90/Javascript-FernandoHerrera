82bce79441f0694cec609baabcca8c990a53681d
Martin PCescritorio <bergesio90@gmail.com>
2023-04-16 18:57:54 +0200

    170. Comportamiento esperado del modal

--- 07-http-app/src/users/presentation/render-modal/render-modal.js
+++ 07-http-app/src/users/presentation/render-modal/render-modal.js
@@ -1,7 +1,17 @@
 import modaHtml from './render-modal.html?raw';
 import './render-modal.css';
 
-let modal;
+let modal, form;
+
+//TODO cargar usuario por id
+export const showModal = () => {
+	modal?.classList.remove('hide-modal');
+};
+
+export const hideModal = () => {
+	modal?.classList.add('hide-modal');
+	//TODO reset del formulario
+};
 
 /**
  *
@@ -13,6 +23,17 @@ export const renderModal = (element) => {
 	modal = document.createElement('div');
 	modal.innerHTML = modaHtml;
 	modal.className = 'modal-container hide-modal';
+	form = modal.querySelector('form');
+
+	modal.addEventListener('click', (event) => {
+		if (event.target.className === 'modal-container') {
+			hideModal();
+		}
+	});
+
+	form.addEventListener('submit', (event) => {
+		event.preventDefault();
+	});
 
 	element.append(modal);
 };
