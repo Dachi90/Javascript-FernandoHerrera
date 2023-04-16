64279d38456d51867cb3e217d8758c8cf47c9739
Martin PCescritorio <bergesio90@gmail.com>
2023-04-16 12:00:58 +0200

    168. Botón flotante

--- 07-http-app/src/users/users-app.js
+++ 07-http-app/src/users/users-app.js
@@ -1,4 +1,5 @@
 import { renderButtons } from './presentation/reder-button/render-button';
+import { renderAddButton } from './presentation/render-add-button/render-add-button';
 import { renderTable } from './presentation/render-table/render-table';
 import usersStore from './store/users.store';
 
@@ -13,4 +14,5 @@ export const UserApp = async (element) => {
 
 	renderTable(element);
 	renderButtons(element);
+	renderAddButton(element);
 };
