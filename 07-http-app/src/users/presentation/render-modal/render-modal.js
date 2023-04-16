a69ec88813a51eaec300ac14f7b43eab68c55988
Martin PCescritorio <bergesio90@gmail.com>
2023-04-16 19:47:59 +0200

    171. Tomar la dara del formulario

--- 07-http-app/src/users/presentation/render-modal/render-modal.js
+++ 07-http-app/src/users/presentation/render-modal/render-modal.js
@@ -10,7 +10,7 @@ export const showModal = () => {
 
 export const hideModal = () => {
 	modal?.classList.add('hide-modal');
-	//TODO reset del formulario
+	form?.reset();
 };
 
 /**
@@ -33,6 +33,24 @@ export const renderModal = (element) => {
 
 	form.addEventListener('submit', (event) => {
 		event.preventDefault();
+		const formData = new FormData(form);
+		const userLike = {};
+		for (const [key, value] of formData) {
+			if (key === 'balance') {
+				userLike[key] = Number(value);
+				continue;
+			}
+
+			if (key === 'isActive') {
+				userLike[key] = value === 'on' ? true : false;
+				continue;
+			}
+			userLike[key] = value;
+		}
+		//console.log(userLike);
+		//TODO guardar usuario
+
+		hideModal;
 	});
 
 	element.append(modal);
